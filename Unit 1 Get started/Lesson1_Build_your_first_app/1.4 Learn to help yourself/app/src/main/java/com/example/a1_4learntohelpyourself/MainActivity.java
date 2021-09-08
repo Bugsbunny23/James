c6c387228ea04eb7be3f0c44224d9a3d0ce74f8a
sgm28 <sgm28@njit.edu>
2021-09-08 16:32:44 -0400

    docs: Create two folder: Lesson2..., Activities_and_intents app

--- Unit 1 Get started/Lesson1_Build_your_first_app/1.4 Learn to help yourself/app/src/main/java/com/example/a1_4learntohelpyourself/MainActivity.java
+++ Unit 1 Get started/Lesson1_Build_your_first_app/1.4 Learn to help yourself/app/src/main/java/com/example/a1_4learntohelpyourself/MainActivity.java
@@ -1,34 +1,31 @@
 package com.example.a1_4learntohelpyourself;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+        
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -43,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
